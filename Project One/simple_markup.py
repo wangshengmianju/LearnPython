a66820b9960cb07c3ac768258bab5700446caf12
yefei <396968776@qq.com>
2019-12-10 16:38:09 +0800

    complete the annotation for the first implementation

--- Project One/simple_markup.py
+++ Project One/simple_markup.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import sys, re
 from util import *
 
--- Project One/util.py
+++ Project One/util.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
+
 #To find out the block
 #A generator to form text block
-def lines(file):      
-    r"yeild lines in file, add \n after last line "#为什么必须要在最后加上一个空行
+def lines(file):      #为什么最后一行一定要加上空行：因为block函数候采用了elif：block的判断来作为生成文本块的信号
+    r'yeild lines in file, add \n after last line'
     for line in file: yield line
     yield '\n'
 
