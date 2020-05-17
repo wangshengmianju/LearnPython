4ec94f194ea0d2d6de08aa1d183cf2e7968d097b
Yefei <396968776@qq.com>
2020-05-17 15:29:30 +0800

    learn GUI with tk

---
+++ TEST/winEditor.py
@@ -0,0 +1,25 @@
+from tkinter import *
+from tkinter.scrolledtext import ScrolledText
+
+def load():
+    with open(filename.get()+'.txt') as file:
+        contents.delete('1.0',END)
+        contents.insert(INSERT,file.read())
+
+def save():
+    with open(filename.get()+'.txt','w') as file:
+        file.write(contents.get('1.0',END))
+
+top = Tk()
+top.title('Simple Editor')
+
+contents = ScrolledText()
+contents.pack(side=BOTTOM, expand=True, fill=BOTH)
+
+filename = Entry()
+filename.pack(side=LEFT, expand=TRUE, fill=X)
+
+Button(text='Open',command=load).pack(side=LEFT)
+Button(text='save',command=save).pack(side=LEFT)
+
+mainloop()
